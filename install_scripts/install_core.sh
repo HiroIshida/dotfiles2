5430525da77b3ca5958bc2e0993203a6b1660f8d
HiroIshida <spitfire.docomo@gmail.com>
2021-11-04 07:34:19 +0900

    Install gnome-tweak-tool

--- install_scripts/install_core.sh
+++ install_scripts/install_core.sh
@@ -5,6 +5,9 @@ sudo apt install vim-gtk3 -y
 sudo apt install tmux -y 
 sudo apt install cmake -y
 
+# setup ubuntu 18~20 workspace (open gnome-tweak and then choose static workspace)
+sudo apt install -y gnome-tweak-tool 
+
 # install albert
 wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_$(lsb_release -sr)/Release.key -O Release.key
 sudo apt-key add - < Release.key
