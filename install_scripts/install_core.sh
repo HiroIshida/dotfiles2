efb5da5356b924dad5639e773b19ec075dc3f9ac
HiroIshida <spitfire.docomo@gmail.com>
2021-11-04 06:45:20 +0900

    Apt install core

---
+++ install_scripts/install_core.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt update
+sudo apt install git -y
+sudo apt install vim-gtk3 -y 
+sudo apt install tmux -y 
+sudo apt install cmake -y
+
+# install albert
+wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_$(lsb_release -sr)/Release.key -O Release.key
+sudo apt-key add - < Release.key
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_$(lsb_release -sr)/ /' > /etc/apt/sources.list.d/home:manuelschneid3r.list"
+sudo apt update 
+sudo apt install albert -y --allow-unauthenticated
