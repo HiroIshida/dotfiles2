29fe48236d7c62959e9adda127e9227351d34e7b
HiroIshida <spitfire.docomo@gmail.com>
2021-11-05 20:17:29 +0900

    Install latest nodejs (for pyright-server)

---
+++ install_scripts/install_latest_nodejs.sh
@@ -0,0 +1,6 @@
+# https://askubuntu.com/questions/426750/how-can-i-update-my-nodejs-to-the-latest-version
+sudo apt install npm -y
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stable
+sudo n latest
