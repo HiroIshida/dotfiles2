694089fede4b80e70a572e210a654d4466030999
HiroIshida <spitfire.docomo@gmail.com>
2022-04-05 23:12:48 +0900

    Remove albert

--- install_scripts/install_core.sh
+++ install_scripts/install_core.sh
@@ -8,10 +8,3 @@ sudo apt install silversearcher-ag -y
 
 # setup ubuntu 18~20 workspace (open gnome-tweak and then choose static workspace)
 sudo apt install -y gnome-tweak-tool 
-
-# install albert
-wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_$(lsb_release -sr)/Release.key -O Release.key
-sudo apt-key add - < Release.key
-sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_$(lsb_release -sr)/ /' > /etc/apt/sources.list.d/home:manuelschneid3r.list"
-sudo apt update 
-sudo apt install albert -y --allow-unauthenticated
