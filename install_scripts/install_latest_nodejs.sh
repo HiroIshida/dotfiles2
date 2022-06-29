eb1d0a61ee88d7d8849e96d2a0bd3c75ae1578ad
HiroIshida <spitfire.docomo@gmail.com>
2022-06-29 22:01:27 +0900

    Install npm deps

--- install_scripts/install_latest_nodejs.sh
+++ install_scripts/install_latest_nodejs.sh
@@ -1,4 +1,5 @@
 # https://askubuntu.com/questions/426750/how-can-i-update-my-nodejs-to-the-latest-version
+sudo apt-get install nodejs-dev node-gyp libssl1.0-dev
 sudo apt install npm -y
 sudo npm cache clean -f
 sudo npm install -g n
