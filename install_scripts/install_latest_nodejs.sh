a5dbff21b32cccbfbf5df2a4bab39e10758f332a
HiroIshida <spitfire.docomo@gmail.com>
2022-08-31 09:59:35 +0900

    Do not install npm if using melodic

--- install_scripts/install_latest_nodejs.sh
+++ install_scripts/install_latest_nodejs.sh
@@ -1,9 +1,13 @@
 # https://askubuntu.com/questions/426750/how-can-i-update-my-nodejs-to-the-latest-version
-sudo apt-get install nodejs-dev node-gyp libssl1.0-dev
-sudo apt install npm -y
-sudo npm cache clean -f
-sudo npm install -g n
-sudo n stable
-sudo n latest
+
+# NOTE: this uninstall melodic
+if [ ! -d "/opt/ros/melodic" ]; then
+    sudo apt-get install nodejs-dev node-gyp libssl1.0-dev
+    sudo apt install npm -y
+    sudo npm cache clean -f
+    sudo npm install -g n
+    sudo n stable
+    sudo n latest
+fi
 
 echo "NOTE by Ishida: maybe you need to add 104.16.20.35 registry.npmjs.org to /etc/hosts"
